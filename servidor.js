3884fa010c792134f855553563b4df48dc8b47f9
MARIA EDUARDA BISCUOLA MARCHI <26172476@edu119.local>
2026-09-17 09:55:51 -0300

    Subindo

---
+++ servidor.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const app = express();
+
+let ALUNOS = [
+  { id: 1, nome: "Bernardo", curso: "Desenvolvimento de sistemas" },
+  { id: 2, nome: "Camily", curso: "Redes de computadores" },
+  { id: 3, nome: "Kaue", curso: "Banco de dados" },
+  { id: 4, nome: "Maria", curso: "Administração" },
+  { id: 5, nome: "Marjory", curso: "Desenvolvimento de sistemas" },
+]; 
+
+app.get("/",(req,res)=>{
+    res.json({
+        message: "API alunos funcionando"
+
+    })
+}) 
+
+app.get("/alunos",(req,res) =>{
+    res.json(ALUNOS);
+});
+
+const PORTA = 3000;
+app.listen(PORTA,()=>{
+    console.log("Servidor iniciado com sucesso");
+    console.log(`http://localhost:${PORTA}`);
+})
